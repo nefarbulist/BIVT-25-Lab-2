96781b23697832200c03dedff89f39610ae48a5b
Маслюк Ксения <108191431+nefarbulist@users.noreply.github.com>
2025-10-05 21:29:13 +0300

    Update White.cs

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -1,90 +1,58 @@
-﻿namespace Lab2
+namespace Lab2
 {
     public class White
     {
         const double E = 0.0001;
+
         public int Task1(int n)
         {
             int answer = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                answer += 2 + 3 * i;
+            }
             // end
 
             return answer;
         }
+
         public double Task2(int n)
         {
             double answer = 0;
 
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 1.0 / i;
+            }
             // end
 
             return answer;
         }
+
         public long Task3(int n)
         {
-            long answer = 0;
+            long answer = 1;
 
             // code here
-
+            for (int i = 2; i <= n; i++)
+            {
+                answer *= i;
+            }
             // end
 
             return answer;
         }
+
         public long Task4(int a, int b)
         {
-            long answer = 0;
+            if (a == 0 && b == 0) return 0;
+            if (b == 0) return 1;
+
+            long answer = 1;
 
             // code here
-
-            // end
-
-            return answer;
-        }
-        public int Task5(int L)
-        {
-            int answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
-        }
-        public double Task6(double x)
-        {
-            double answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
-        }
-
-        public int Task7(int n)
-        {
-            int answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
-        }
-        public int Task8(double L, double v)
-        {
-            int answer = 0;
-            const double R = 6371.0; // радиус Земли, км
-
-            // code here
-
-            // end
-
-            return answer;
-        }
-    }
-
-}
+            for (int i = 0; i < b; i++)
+            {
