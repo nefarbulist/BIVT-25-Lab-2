3264166f6ae5ef218a83a6d02d0a685610b6e8ff
Маслюк Ксения <108191431+nefarbulist@users.noreply.github.com>
2025-10-05 22:33:13 +0300

    Update White.cs

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -1,90 +1,85 @@
-﻿namespace Lab2
+namespace Lab2
 {
     public class White
     {
         const double E = 0.0001;
+
         public int Task1(int n)
         {
             int answer = 0;
-
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 3 * i - 1;
+            }
             // end
-
             return answer;
         }
+
         public double Task2(int n)
         {
             double answer = 0;
-
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 1.0 / i;
+            }
             // end
-
             return answer;
         }
+
         public long Task3(int n)
         {
-            long answer = 0;
-
+            long answer = 1;
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer *= i;
+            }
             // end
-
             return answer;
         }
+
         public long Task4(int a, int b)
         {
-            long answer = 0;
-
+            long answer = 1;
             // code here
-
+            for (int i = 0; i < b; i++)
+            {
+                answer *= a;
+            }
             // end
-
             return answer;
         }
+
         public int Task5(int L)
         {
             int answer = 0;
-
             // code here
-
+            long prod = 1;
+            int n = 1;
+            while (prod <= L)
+            {
+                prod *= n;
+                n += 3;
+            }
+            answer = n - 3;
             // end
-
-            return answer;
-        }
-        public double Task6(double x)
-        {
-            double answer = 0;
-
-            // code here
-
-            // end
-
             return answer;
         }
 
         public int Task7(int n)
         {
             int answer = 0;
-
             // code here
-
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
             // end
-
-            return answer;
-        }
-        public int Task8(double L, double v)
-        {
-            int answer = 0;
-            const double R = 6371.0; // радиус Земли, км
-
-            // code here
-
-            // end
-
             return answer;
         }
     }
-
 }
